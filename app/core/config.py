d30610eb782a9fd8e4439f3eee5be67cf9b27f2c
Rafał Lelito <rafal.lelito@onet.com.pl>
2026-02-08 22:38:04 +0100

    Created config.py % logging.py

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str
+    redis_url: str
+    risk_threshold_block: int
+    risk_threshold_review: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
